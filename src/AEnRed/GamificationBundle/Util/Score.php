dac2465519e487ae55834aaa42e2900dcb9b80f8
Luis Murias <murias@aenred.com>
2012-12-02 11:43:55 +0100

    Add Score util class

---
+++ src/AEnRed/GamificationBundle/Util/Score.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Util class to manage the score
+ *
+ * @author Luis
+ */
+class Score {
+    
+    /**
+     * Return the level in function of score
+     * 
+     * @param int $score
+     * @return int A resultant level
+     */
+    public function getLevel($score){
+        $level = $score;
+        return $level;
+    }
+    
+    
+}
+
+?>
